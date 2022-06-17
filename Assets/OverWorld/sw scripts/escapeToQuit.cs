f6328b8547d6d5b99ff336fdc8dfb854787b5222
Casey Farina <caseyfarina@gmail.com>
2022-06-17 13:09:49 -0700

    backup
    backup

---
+++ Assets/OverWorld/sw scripts/escapeToQuit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+// Quits the player when the user hits escape
+
+public class escapeToQuit : MonoBehaviour
+{
+	public float restartHeight = -10f;
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+	
+    }
+}
